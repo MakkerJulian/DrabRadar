d4c75bf0adfcc3f34c7a7f0c0ed90205cd2695ed
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-05 16:27:31 +0100

    added seeding support for countries

--- Backend/drab-backend/src/api/country/services/country/country.service.ts
+++ Backend/drab-backend/src/api/country/services/country/country.service.ts
@@ -2,15 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Country } from 'src/typeorm/country.entity';
 import { Repository } from 'typeorm';
+import { countries } from 'src/seed';
 
 @Injectable()
 export class CountryService {
-    constructor(
-        @InjectRepository(Country)
-        private readonly countryRepository: Repository<Country>,
-    ) {}
+  constructor(
+    @InjectRepository(Country)
+    private readonly countryRepository: Repository<Country>,
+  ) {}
 
-    getCountry() {
-        return this.countryRepository.find()
-    }
+  getCountry() {
+    return this.countryRepository.find();
+  }
+
+  async seedCountries() {
+    const newCountries = countries.map((country) => {
+      return {
+        code: country.country_code,
+        name: country.country,
+      };
+    });
+    await this.countryRepository.save(newCountries);
+    return this.countryRepository.find();
+  }
 }
---
+++ Backend/drab-backend/src/seed/index.ts
@@ -0,0 +1,7 @@
+import countries from './countries.json';
+// import geolocation from './geolocation.json';
+// import station from './stations.json';
+// import nearestlocation from './nearestlocation.json';
+
+// export { countries, geolocation, station, nearestlocation };
+export { countries };
