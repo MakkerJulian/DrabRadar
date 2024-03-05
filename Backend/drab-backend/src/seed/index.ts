10b23ff8bc116a779ae220802fbddf0d4904779d
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-05 16:47:47 +0100

    support for all seeding

--- Backend/drab-backend/src/seed/index.ts
+++ Backend/drab-backend/src/seed/index.ts
@@ -1,7 +1,7 @@
 import countries from './countries.json';
-// import geolocation from './geolocation.json';
-// import station from './stations.json';
-// import nearestlocation from './nearestlocation.json';
+import geolocation from './geolocation.json';
+import station from './stations.json';
+import nearestlocation from './nearestlocation.json';
+import nearestlocation2 from './nearestlocation2.json';
 
-// export { countries, geolocation, station, nearestlocation };
-export { countries };
+export { countries, station, nearestlocation, nearestlocation2, geolocation };
