19584e3954faacb3802637dc99aab5f21901a041
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-06 10:30:43 +0100

    Update weatherstation.module.ts

--- Backend/drab-backend/src/api/weatherstation/weatherstation.module.ts
+++ Backend/drab-backend/src/api/weatherstation/weatherstation.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WeatherstationController } from './controllers/weatherstation/weatherstation.controller';
 import { WeatherstationService } from './services/weatherstation/weatherstation.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Weatherstation } from 'src/typeorm/weatherstation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Weatherstation])],
+  exports: [WeatherstationService],
   controllers: [WeatherstationController],
-  providers: [WeatherstationService]
+  providers: [WeatherstationService],
 })
 export class WeatherstationModule {}
