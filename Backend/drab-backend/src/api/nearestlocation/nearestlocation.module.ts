190771438ca664d97e7b467df195186e8354b078
KajTV <Kajtervelde@gmail.com>
2024-03-06 10:22:44 +0100

    forgot typeorm import

--- Backend/drab-backend/src/api/nearestlocation/nearestlocation.module.ts
+++ Backend/drab-backend/src/api/nearestlocation/nearestlocation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NearestlocationController } from './controllers/nearestlocation/nearestlocation.controller';
 import { NearestlocationService } from './services/nearestlocation/nearestlocation.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NearestLocation } from 'src/typeorm/nearestlocation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NearestLocation])],
   controllers: [NearestlocationController],
   providers: [NearestlocationService]
 })
