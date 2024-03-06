23eb3f2be01ff820e92f73f3de7d1d79c5adb410
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-06 08:49:27 +0100

    added all geolocations

--- Backend/drab-backend/src/seed/index.ts
+++ Backend/drab-backend/src/seed/index.ts
@@ -1,7 +1,20 @@
 import countries from './countries.json';
 import geolocation from './geolocation.json';
+import geolocation2 from './geolocation2.json';
+import geolocation3 from './geolocation3.json';
+import geolocation4 from './geolocation4.json';
+
 import station from './stations.json';
 import nearestlocation from './nearestlocation.json';
 import nearestlocation2 from './nearestlocation2.json';
 
-export { countries, station, nearestlocation, nearestlocation2, geolocation };
+export {
+  countries,
+  station,
+  nearestlocation,
+  nearestlocation2,
+  geolocation,
+  geolocation2,
+  geolocation3,
+  geolocation4,
+};
