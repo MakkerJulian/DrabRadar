ae914ff9f5226b3d91c2c5b7179e50ecb58d9bea
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-06 10:44:15 +0100

    werkend

--- Backend/drab-backend/src/app.service.ts
+++ Backend/drab-backend/src/app.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CountryService } from './api/country/services/country/country.service';
-import { GeolocationService } from './api/geolocation/services/geolocation/geolocation.service';
+// import { GeolocationService } from './api/geolocation/services/geolocation/geolocation.service';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly countryService: CountryService,
-    private readonly geolocationService: GeolocationService,
+    // private readonly geolocationService: GeolocationService,
   ) {
     this.seedDatabase();
   }
 
   async seedDatabase() {
     await this.countryService.seedCountries();
-    await this.geolocationService.seedGeoLocations();
+    // await this.geolocationService.seedGeoLocations();
   }
 }
