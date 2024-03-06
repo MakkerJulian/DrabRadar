70c4fc2c4c64f7c8c4744eacbffada89f7257de1
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-06 09:33:43 +0100

    lint

--- Backend/drab-backend/src/api/geolocation/controllers/geolocation/geolocation.controller.ts
+++ Backend/drab-backend/src/api/geolocation/controllers/geolocation/geolocation.controller.ts
@@ -1,19 +1,12 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    UsePipes,
-    ValidationPipe,
-  } from '@nestjs/common';
-  import { GeolocationService } from '../../services/geolocation/geolocation.service';
-  
-  @Controller('geolocation')
-  export class GeolocationController {
-    constructor(private readonly geolocationService: GeolocationService) {}
-  
-    @Get()
-    getGeolocation() {
-      return this.geolocationService.getGeolocation();
-    }
+import { Controller, Get } from '@nestjs/common';
+import { GeolocationService } from '../../services/geolocation/geolocation.service';
+
+@Controller('geolocation')
+export class GeolocationController {
+  constructor(private readonly geolocationService: GeolocationService) {}
+
+  @Get()
+  getGeolocation() {
+    return this.geolocationService.getGeolocation();
   }
+}
--- Backend/drab-backend/src/api/geolocation/services/geolocation/geolocation.service.ts
+++ Backend/drab-backend/src/api/geolocation/services/geolocation/geolocation.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { geolocation, geolocation2, geolocation3, geolocation4 } from 'src/seed';
+import {
+  geolocation,
+  geolocation2,
+  geolocation3,
+  geolocation4,
+} from 'src/seed';
 import { Geolocation } from 'src/typeorm/geolocation.entity';
 import { Repository } from 'typeorm';
 
@@ -18,26 +23,33 @@ export class GeolocationService {
     await this.seedGeoLocations(geolocation4);
     return this.geolocationRepository.find({ relations: ['country'] }); 
   }
-  async seedGeoLocations(geolocations ){
+  async seedGeoLocations(geolocations) {
     const newGeoLocations = geolocations.map((geolocation) => {
       return {
-        name : geolocation.station_name,
-        country : geolocation.country_code,
-        island : geolocation.island ==="N/A"?null: geolocation.island,
-        county : geolocation.county ==="N/A"?null: geolocation.county,
-        place : geolocation.place ==="N/A"?null: geolocation.place,
-        hamlet : geolocation.hamlet ==="N/A"?null: geolocation.hamlet,
-        town : geolocation.town ==="N/A"?null: geolocation.town,
-        municipality : geolocation.municipality ==="N/A"?null: geolocation.municipality,
-        state_district : geolocation.state_district ==="N/A"?null: geolocation.state_district,
-        administrative : geolocation.administrative ==="N/A"?null: geolocation.administrative,
-        state : geolocation.state ==="N/A"?null: geolocation.state,
-        village : geolocation.village ==="N/A"?null: geolocation.village,
-        region : geolocation.region ==="N/A"?null: geolocation.region,
-        province : geolocation.province ==="N/A"?null: geolocation.province,
-        city : geolocation.city ==="N/A"?null: geolocation.city,
-        locality : geolocation.locality ==="N/A"?null: geolocation.locality,
-        postcode : geolocation.postcode ==="N/A"?null: geolocation.postcode,
+        name: geolocation.station_name,
+        country: geolocation.country_code,
+        island: geolocation.island === 'N/A' ? null : geolocation.island,
+        county: geolocation.county === 'N/A' ? null : geolocation.county,
+        place: geolocation.place === 'N/A' ? null : geolocation.place,
+        hamlet: geolocation.hamlet === 'N/A' ? null : geolocation.hamlet,
+        town: geolocation.town === 'N/A' ? null : geolocation.town,
+        municipality:
+          geolocation.municipality === 'N/A' ? null : geolocation.municipality,
+        state_district:
+          geolocation.state_district === 'N/A'
+            ? null
+            : geolocation.state_district,
+        administrative:
+          geolocation.administrative === 'N/A'
+            ? null
+            : geolocation.administrative,
+        state: geolocation.state === 'N/A' ? null : geolocation.state,
+        village: geolocation.village === 'N/A' ? null : geolocation.village,
+        region: geolocation.region === 'N/A' ? null : geolocation.region,
+        province: geolocation.province === 'N/A' ? null : geolocation.province,
+        city: geolocation.city === 'N/A' ? null : geolocation.city,
+        locality: geolocation.locality === 'N/A' ? null : geolocation.locality,
+        postcode: geolocation.postcode === 'N/A' ? null : geolocation.postcode,
       };
     });
     await this.geolocationRepository.save(newGeoLocations);
