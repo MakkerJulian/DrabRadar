656ac1d7c31f17ab38e147c20fa3f891c266a4dd
david-warris <davidwarris@outlook.com>
2024-03-15 15:52:33 +0100

    Update Login.tsx

--- Frontend/Drabfrontend/src/pages/Login.tsx
+++ Frontend/Drabfrontend/src/pages/Login.tsx
@@ -62,6 +62,8 @@ export const Login = () => {
                 objectFit:"fill"
             }}></img>
             <Box
+                boxShadow={'0 0 10px rgba(51, 153, 255, 2)'}
+                borderRadius={'25px'}
                 minWidth={'25%'} 
                 maxWidth={'30%'}
                 margin={'auto'}
@@ -72,6 +74,7 @@ export const Login = () => {
                 flexDirection={'column'}
                 alignItems={'center'}
                 onSubmit={() => handlePost()}
+                
             >
                 <Typography 
                     variant={'h3'} 
