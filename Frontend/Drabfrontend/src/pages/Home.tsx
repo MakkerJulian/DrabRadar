7b849f28839eb03e78256a6f1606048496166774
david-warris <davidwarris@outlook.com>
2024-03-18 23:29:25 +0100

    added new center and overlay

--- Frontend/Drabfrontend/src/pages/Home.tsx
+++ Frontend/Drabfrontend/src/pages/Home.tsx
@@ -5,16 +5,21 @@ import '../styling/Map.css';
 
 export const Home = () => {
     return (
-        <MapContainer center={[53.153180193809376, 6.992662913697204]} zoom={3} style={{"height": "960px"}}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[53.153180193809376, 6.992662913697204]}>
-                <Popup>
-                    Kars Doxx
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div className="map-wrapper">
+            <MapContainer center={[39.17349868578113, 40.38833747608593]} zoom={2.5} style={{"height": "960px"}}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[53.153180193809376, 33.38833747608593]}>
+                    <Popup>
+                        Kars Doxx
+                    </Popup>
+                </Marker>
+            </MapContainer>
+            <div className="extra-div">
+                hi
+            </div>
+        </div>
     );
 }
