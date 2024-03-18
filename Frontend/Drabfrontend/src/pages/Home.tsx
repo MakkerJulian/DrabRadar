63fd0d126d09c0c20fbfb6732899a95e77ec2767
Kars <97431994+Karscoding@users.noreply.github.com>
2024-03-18 23:17:28 +0100

    Dark mode

--- Frontend/Drabfrontend/src/pages/Home.tsx
+++ Frontend/Drabfrontend/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import '../styling/Map.css';
 
 export const Home = () => {
     return (
-        <MapContainer center={[53.153180193809376, 6.992662913697204]} zoom={13} style={{"height": "900px"}}>
+        <MapContainer center={[53.153180193809376, 6.992662913697204]} zoom={3} style={{"height": "960px"}}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
