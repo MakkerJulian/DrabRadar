3c8d306b16bc46d09a7c26534f20327df7cc859f
Kars <97431994+Karscoding@users.noreply.github.com>
2024-03-18 22:44:55 +0100

    Map implementation
    Co-Authored-By: david <128608284+david-warris@users.noreply.github.com>

--- Frontend/Drabfrontend/src/pages/Home.tsx
+++ Frontend/Drabfrontend/src/pages/Home.tsx
@@ -1,12 +1,19 @@
-import { Box, Typography } from '@mui/material';
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export const Home = () => {
     return (
-        <Box>
-            <Typography>
-                heyhoi, dit is de drabsite
-            </Typography>
-        </Box>
+        <MapContainer center={[53.153180193809376, 6.992662913697204]} zoom={13} style={{"height": "900px"}}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[53.153180193809376, 6.992662913697204]}>
+                <Popup>
+                    Kars Doxx
+                </Popup>
+            </Marker>
+        </MapContainer>
     );
 }
