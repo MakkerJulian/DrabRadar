b29ca1ef8572ef0e430c0d31dba20d9d86caf865
MakkerJulian <juliankloosterhuis@gmail.com>
2024-03-22 10:49:29 +0100

    begin page

---
+++ Frontend/Drabfrontend/src/pages/WeatherStations.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const WeatherStations = () => {
+
+
+    return (
+        <div>
+            <h1>Weather Stations</h1>
+        </div>
+    );
+}
--- Frontend/Drabfrontend/src/routes.tsx
+++ Frontend/Drabfrontend/src/routes.tsx
@@ -6,28 +6,29 @@ import { Sales } from "./pages/Sales";
 interface Route {
     path: string;
     name: string;
-    element: ()=>JSX.Element;
+    element: () => JSX.Element;
 }
 
 export const routes: Route[] = [
     {
-        name:"home",
-        path:"/",
+        name: "home",
+        path: "/",
         element: Home,
     },
     {
-        name:'login',
-        path:'/login',
+        name: 'login',
+        path: '/login',
         element: Login,
     },
     {
-        name:'sales',
-        path:'/sales',
+        name: 'sales',
+        path: '/sales',
         element: Sales
     },
     {
         name: 'customer',
         path: '/customer/:id',
         element: CustomerDetails
-    }
+    },
+
 ];
