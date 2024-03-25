e04c3644826cc4938079d0148f2725afd2c517c7
KajTV <Kajtervelde@gmail.com>
2024-03-25 14:51:45 +0100

    Create Accounts from admin

--- Frontend/Drabfrontend/src/pages/Admin.tsx
+++ Frontend/Drabfrontend/src/pages/Admin.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
-import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axiosInstance from "../axios";
 import { Account, AccountCreate } from "../types";
 import type { GridColDef } from "@mui/x-data-grid";
 import { DataGrid } from "@mui/x-data-grid";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import { useNavigate } from "react-router-dom";
 import { CustomModal } from "../components/customModal";
 import { enqueueSnackbar } from "notistack";
 import { useForm } from "react-hook-form";
@@ -20,6 +18,25 @@ const emptyForm: AccountCreate = {
 
 export const Admin = () => {
     const [accounts, setAccounts] = React.useState<Account[]>([]);
+    const [openAccount, setOpenAccount] = React.useState<boolean>(false);
+    const [form, setForm] = React.useState<AccountCreate>(emptyForm);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const createAccount = () => {
+        axiosInstance.post('/account', form).then(() => {
+            enqueueSnackbar('Account created', { variant: 'success' });
+            setOpenAccount(false);
+            axiosInstance.get<Account[]>('/account').then((response) => {
+                setAccounts(response.data);
+            });
+        }).catch(() => {
+            enqueueSnackbar('Failed to create account', { variant: 'error' });
+        });
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
     const columns: GridColDef[] = [
         {
             field: 'id', flex: 0.5, headerName: 'ID',
@@ -52,6 +69,15 @@ export const Admin = () => {
                 Admin
             </Typography>
 
+            <Button
+                sx={{ backgroundColor: 'blue', color: 'white' }}
+                onClick={() => {
+                    setOpenAccount(!openAccount);
+                }}
+            >
+                Add new account
+            </Button>
+
             <DataGrid
                 rows={accounts}
                 columns={columns}
@@ -63,6 +89,76 @@ export const Admin = () => {
                 }}
             >
             </DataGrid>
+
+            <CustomModal
+                open={openAccount}
+                title="Add new account"
+                setOpen={setOpenAccount}
+                onSubmit={handleSubmit(createAccount)}
+            >
+                <TextField
+                    sx={{ width: '50%', margin: '20px' }}
+                    label="Name"
+                    value={form.name}
+                    {...register('name', { required: "name can't be empty", minLength: { value: 5, message: "name must be at least 5 characters" } })}
+                    onChange={handleChange}
+                    helperText={errors.name?.message?.toString()}
+                    error={errors.name?.message !== undefined}
+                >
+                </TextField>
+
+                <TextField
+                    sx={{ width: '50%', margin: '20px' }}
+                    label="E-mail"
+                    value={form.email}
+                    {...register('email', {
+                        required: "E-mail can't be empty", pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "invalid email address"
+                        }
+                    })}
+                    onChange={handleChange}
+                    helperText={errors.email?.message?.toString()}
+                    error={errors.email?.message !== undefined}
+                >
+                </TextField>
+
+
+                <TextField
+                    sx={{ width: '50%', margin: '20px' }}
+                    label="Phone number"
+                    type='phone'
+                    value={form.phone}
+                    {...register('phone', { required: "phone number can't be empty" })}
+                    onChange={handleChange}
+                    helperText={errors.phone?.message?.toString()}
+                    error={errors.phone?.message !== undefined}
+                >
+                </TextField>
+
+                <TextField
+                    sx={{ width: '50%', margin: '20px' }}
+                    label="Password"
+                    value={form.password}
+                    {...register('password', { required: "password can't be empty" })}
+                    onChange={handleChange}
+                    helperText={errors.password?.message?.toString()}
+                    error={errors.password?.message !== undefined}
+                >
+                </TextField>
+
+                <TextField
+                    sx={{ width: '50%', margin: '20px' }}
+                    label="Role"
+                    value={form.role}
+                    {...register('role', { required: "role can't be empty" })}
+                    onChange={handleChange}
+                    helperText={errors.role?.message?.toString()}
+                    error={errors.role?.message !== undefined}
+                >
+                </TextField>
+            </CustomModal>
+
         </Box>
     )
 }
