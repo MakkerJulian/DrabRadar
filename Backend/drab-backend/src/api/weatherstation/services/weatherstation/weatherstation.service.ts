edb1a412e5eb26a27ca23494d816995890123433
Koninglukaas <Lucasbruins@outlook.com>
2024-04-03 15:11:20 +0200

    afx2

--- Backend/drab-backend/src/api/weatherstation/services/weatherstation/weatherstation.service.ts
+++ Backend/drab-backend/src/api/weatherstation/services/weatherstation/weatherstation.service.ts
@@ -56,47 +56,5 @@ export class WeatherstationService {
     });
     this.weatherstationRepository.save(newWeatherstations);
   }
-  // async getExtern(token: string, latitude: number, longitude: number, elevation: number) {
-  //   const subscription = await this.subscriptionService.getByToken(token);
-  //   let allowedStations = subscription.contracts.map(contract => {
-  //     return contract.weatherstations;
-  //   }).flat();
-
-  //   const lat = latitude ?? 0;
-  //   const long = longitude ?? 0;
-  //   const elev = elevation ?? 0;
-
-  //   // filters toepassen
-  //   allowedStations = allowedStations.filter(station => {
-  //     return station.latitude <= lat;
-  //     //Filterd alle stations die een latitude hebben die kleiner is dan de gegeven latitude, 
-  //     //dus returnt alle stations die ten noorden van de gegeven latitude liggen
-  //   });
-
-  //   allowedStations = allowedStations.filter(station => {
-  //     return station.longitude <= long;
-  //   });
-
-
-  //   allowedStations = allowedStations.filter(station => {
-  //     return station.elevation <= elev;
-  //   });
-  //   console.log(allowedStations);
-
-
-  //   //Loop door alle stations heen
-  //   // Pak (bvb) de laatse 30 weergegevens van die station met .flat()
-  //   // Return dit
-  //    // Retrieve weather data from WeatherDataService
-  //    const stationData = await Promise.all(allowedStations.map(async station => {
-  //     const latestWeatherData = await this.weatherDataService.getLatestWeatherDataForStation(station);
-  //     return {
-  //       station,
-  //       latestWeatherData,
-  //     };
-  //   }));
-
-  //   return stationData;
-  // }
 }
 
